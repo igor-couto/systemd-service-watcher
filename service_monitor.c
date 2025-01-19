37d7f5e1be93a4b9e2fc364044c9d532c7758d69
Igor Couto <igor.fcouto@gmail.com>
2025-01-19 23:11:41 +0000

    Refactor

--- service_monitor.c
+++ service_monitor.c
@@ -113,6 +113,7 @@ void monitor_services(const ServiceInfo *services, int service_count, unsigned i
             }
             pclose(cmd_fp);
         }
+        
         sleep(interval);
     }
 }
